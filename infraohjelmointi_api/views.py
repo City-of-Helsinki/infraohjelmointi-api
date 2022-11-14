4d7d6eb3dc405130d7065b69bf0c8218f6545034
Kreshnik Gunga <kreshnik.gunga@robu.fi>
2022-11-14 12:13:12 +0200

    Fix to show project and project types

--- infraohjelmointi_api/views.py
+++ infraohjelmointi_api/views.py
@@ -1,23 +1,24 @@
-
 from rest_framework import viewsets
-from infraohjelmointi_api.serializers import ProjectSerializer, ProjectTypeSerializer
+from .serializers import ProjectSerializer, ProjectTypeSerializer
+
 
 class BaseViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.serializer_class.Meta.model.objects.all()
 
+
 class ProjectViewSet(BaseViewSet):
-  """
-  API endpoint that allows projects to be viewed or edited.
-  """
-  permission_classes = []
-  serializer_class = ProjectSerializer
+    """
+    API endpoint that allows projects to be viewed or edited.
+    """
+    permission_classes = []
+    serializer_class = ProjectSerializer
 
 
 class ProjectTypeViewSet(BaseViewSet):
-  """
-  API endpoint that allows project types to be viewed or edited.
-  """
-  permission_classes = []
-  serializer_class = ProjectTypeSerializer
+    """
+    API endpoint that allows project types to be viewed or edited.
+    """
+    permission_classes = []
+    serializer_class = ProjectTypeSerializer
