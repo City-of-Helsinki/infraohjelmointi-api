b0ebdca608a978678efeecee2ab55f3538e3f649
Uzair Ahmed <uzair.ahmed@Uzairs-MacBook-Pro-16.local>
2022-11-19 20:07:20 +0200

    registered models in admin site

--- infraohjelmointi_api/admin.py
+++ infraohjelmointi_api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Project)
+admin.site.register(ProjectArea)
+admin.site.register(ProjectSet)
+admin.site.register(ProjectType)
+admin.site.register(BudgetItem)
+admin.site.register(Task)
+admin.site.register(Person)
+
 
 # Register your models here.
