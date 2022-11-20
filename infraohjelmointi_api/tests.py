30a940e6b66377c2dab3486cf9131ff9ab187e4c
Uzair Ahmed <uzair.ahmed@Uzairs-MacBook-Pro-16.local>
2022-11-21 00:57:41 +0200

    Added new assert statement in POST test for Projects

--- infraohjelmointi_api/tests.py
+++ infraohjelmointi_api/tests.py
@@ -299,8 +299,14 @@ class ProjectTestCase(TestCase):
         self.assertEqual(response.status_code, 201, msg="Status code != 201")
         # deleting id, projectReadiness, createdDate and updatedDate because request body doesn't contain an id and project_readiness but the response does if new resource is created
         res_data = response.json()
+        new_createdId = res_data["id"]
         del res_data["id"]
         del res_data["projectReadiness"]
         del res_data["createdDate"]
         del res_data["updatedDate"]
         self.assertEqual(res_data, data, msg="Created object != POST data")
+        self.assertEqual(
+            Project.objects.filter(id=new_createdId).exists(),
+            True,
+            msg="Project created using POST request does not exist in DB",
+        )
