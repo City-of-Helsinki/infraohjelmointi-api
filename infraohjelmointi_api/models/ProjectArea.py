c2b94f88fc539637cf1023f7c05ed0acd827b3df
Uzair Ahmed <uzair.ahmed@Uzairs-MacBook-Pro-16.local>
2022-11-24 15:54:59 +0200

    Migration made for field alter on ProjectArea model

--- infraohjelmointi_api/models/ProjectArea.py
+++ infraohjelmointi_api/models/ProjectArea.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class ProjectArea(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    areaName = models.CharField(max_length=200, blank=False, null=False)
+    value = models.CharField(max_length=200, blank=False, null=False)
     location = models.CharField(max_length=200, blank=True, null=True)
     createdDate = models.DateTimeField(auto_now_add=True, blank=True)
     updatedDate = models.DateTimeField(auto_now=True, blank=True)
