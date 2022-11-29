32ecebfae4cc6fe0ea2a696137b7f480bb6194b1
Uzair Ahmed <uzair.ahmed@Uzairs-MacBook-Pro-16.local>
2022-11-29 11:24:16 +0200

    Modified test to accomodate new field in Notes table

--- infraohjelmointi_api/tests/test_Notes.py
+++ infraohjelmointi_api/tests/test_Notes.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 import uuid
-from ..models import Person
+from ..models import Person, Project, ProjectArea, ProjectType, ProjectPhase
 from ..models import Note
 from ..serializers import NoteSerializer, NoteHistorySerializer
 from rest_framework.renderers import JSONRenderer
@@ -10,10 +10,18 @@ class NoteTestCase(TestCase):
     note_1_Id = uuid.UUID("5d82c31b-4dee-4e48-be7c-b417e6c5bb9e")
     note_2_Id = uuid.UUID("da4a46f1-2939-43e2-8f92-40e94417813b")
     person_1_Id = uuid.UUID("2c6dece3-cf93-45ba-867d-8f1dd14923fc")
-    person_2_Id = uuid.UUID("9acb1ac2-259e-4300-8cf0-f89c3adaf577")
+    projectTypeId = uuid.UUID("61ddbe61-e013-4bee-abf7-853d389f2b90")
+    projectId = uuid.UUID("5d82c31b-4dee-4e48-be7c-b417e6c5bb9e")
+    sapNetworkIds_1 = [uuid.UUID("1495aaf7-b0af-4847-a73b-7650145a73dc").__str__()]
+    sapProjectIds_1 = [uuid.UUID("e6f0805c-0b20-4248-bfae-21cf6bfe744a").__str__()]
 
     @classmethod
     def setUpTestData(self):
+
+        self.projectType = ProjectType.objects.create(
+            id=self.projectTypeId, value="projectComplex"
+        )
+
         self.person_1 = Person.objects.create(
             id=self.person_1_Id,
             firstName="John",
@@ -22,9 +30,45 @@ class NoteTestCase(TestCase):
             title="Manager",
             phone="0414853275",
         )
+        self.project = Project.objects.create(
+            id=self.projectId,
+            hkrId=43210,
+            sapProject=self.sapProjectIds_1,
+            sapNetwork=self.sapNetworkIds_1,
+            type=self.projectType,
+            name="Test project 1",
+            description="description of the test project",
+            phase=None,
+            programmed=True,
+            constructionPhaseDetail="Current phase is proposal",
+            estPlanningStartYear=2022,
+            estDesignEndYear=2023,
+            estDesignStartDate="2022-11-20",
+            estDesignEndDate="2022-11-28",
+            contractPrepStartDate="2022-11-20",
+            contractPrepEndDate="2022-11-20",
+            warrantyStartDate="2022-11-20",
+            warrantyExpireDate="2022-11-20",
+            perfAmount=20000.00,
+            unitCost=10000.00,
+            costForecast=10000.00,
+            neighborhood="my random neigbhorhood",
+            comittedCost=120.0,
+            tiedCurrYear=12000.00,
+            realizedCost=20.00,
+            spentCost=20000.00,
+            riskAssess="Yes very risky test",
+            priority=None,
+            locked=True,
+            comments="Comments random",
+            delays="yes 1 delay because of tests",
+        )
 
         self.note = Note.objects.create(
-            id=self.note_1_Id, content="Random Note", updatedBy=self.person_1
+            id=self.note_1_Id,
+            content="Random Note",
+            updatedBy=self.person_1,
+            project=self.project,
         )
 
     def test_note_is_created(self):
@@ -137,7 +181,11 @@ class NoteTestCase(TestCase):
         )
 
     def test_POST_note(self):
-        data = {"content": "Random Note POST", "updatedBy": self.person_1_Id.__str__()}
+        data = {
+            "content": "Random Note POST",
+            "updatedBy": self.person_1_Id.__str__(),
+            "project": self.projectId.__str__(),
+        }
         response = self.client.post(
             "/notes/",
             data,
@@ -181,4 +229,13 @@ class NoteTestCase(TestCase):
             Note.objects.filter(id=self.note_1_Id).exists(),
             False,
             msg="Note with Id {} still exists in DB".format(self.note_1_Id),
+        )
+
+    def test_foreign_key_exists(self):
+        self.assertDictEqual(
+            self.project.note_set.all().values()[0],
+            Note.objects.filter(id=self.note_1_Id).values()[0],
+            msg="Project foreign key does not exist in Note with id {}".format(
+                self.note_1_Id
+            ),
         )
