8d6298d52f601fe8c6fce14c35c284d8c262349b
Uzair Ahmed <uzair.ahmed@Uzairs-MacBook-Pro-16.local>
2022-11-30 18:53:13 +0200

    Removed commented code

--- infraohjelmointi_api/views.py
+++ infraohjelmointi_api/views.py
@@ -53,11 +53,6 @@ class ProjectViewSet(BaseViewSet):
             return ProjectGetSerializer
         return ProjectCreateSerializer
 
-    # def partial_update(self, request, pk=None):
-    #     serialized = ProjectCreateSerializer(data=request.data, partial=True)
-    #     serialized.valid()
-    #     return Response(serialized.validated_data, status=status.HTTP_202_ACCEPTED)
-
 
 class TaskStatusViewSet(BaseViewSet):
     """
