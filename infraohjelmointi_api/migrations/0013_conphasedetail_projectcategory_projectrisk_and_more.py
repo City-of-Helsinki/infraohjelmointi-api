15eec1397c398a6cf7ed6d43a1bda5e320cb6328
Uzair Ahmed <uzair.ahmed@Uzairs-MacBook-Pro-16.local>
2022-12-20 09:08:05 +0200

    Migration file edited to rename the table

--- infraohjelmointi_api/migrations/0013_conphasedetail_projectcategory_projectrisk_and_more.py
+++ infraohjelmointi_api/migrations/0013_conphasedetail_projectcategory_projectrisk_and_more.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name="ConPhaseDetail",
+            name="ConstructionPhaseDetail",
             fields=[
                 (
                     "id",
