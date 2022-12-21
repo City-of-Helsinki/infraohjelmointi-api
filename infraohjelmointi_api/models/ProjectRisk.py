277d14f7116d8ad1a2acf8a3851e5a4a26af3088
Uzair Ahmed <uzair.ahmed@Uzairs-MacBook-Pro-16.local>
2022-12-21 13:48:33 +0200

    Altered value field on ProjectRisk model, regenerated migration file to have the change appended to previous DB changes

--- infraohjelmointi_api/models/ProjectRisk.py
+++ infraohjelmointi_api/models/ProjectRisk.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class ProjectRisk(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    value = models.CharField(max_length=200, blank=False, null=False)
+    value = models.CharField(max_length=25, blank=False, null=False)
     createdDate = models.DateTimeField(auto_now_add=True, blank=True)
     updatedDate = models.DateTimeField(auto_now=True, blank=True)
