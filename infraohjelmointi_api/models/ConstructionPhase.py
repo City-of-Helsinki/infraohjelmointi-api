7a8da3de435185eac67eb438dd224f9f4112ffa0
Uzair Ahmed <uzair.ahmed@Uzairs-MacBook-Pro-16.local>
2022-12-21 12:27:42 +0200

    Added constructionWorkQuantity field, renamed ProjectBuildPhase model to ConstructionPhase

--- infraohjelmointi_api/models/ConstructionPhase.py
+++ infraohjelmointi_api/models/ConstructionPhase.py
@@ -2,7 +2,7 @@ import uuid
 from django.db import models
 
 
-class ProjectBuildPhase(models.Model):
+class ConstructionPhase(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     value = models.CharField(max_length=30)
     createdDate = models.DateTimeField(auto_now_add=True, blank=True)
