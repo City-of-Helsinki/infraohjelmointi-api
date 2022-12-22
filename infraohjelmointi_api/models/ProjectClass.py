be9f2579d69b4fff3f5042b785b3feb4f2bd28b5
Uzair Ahmed <uzair.ahmed@Uzairs-MacBook-Pro-16.local>
2022-12-22 17:32:17 +0200

    Changes to migration file and ProjectClass model, name field length changed

--- infraohjelmointi_api/models/ProjectClass.py
+++ infraohjelmointi_api/models/ProjectClass.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class ProjectClass(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=150)
     parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
     createdDate = models.DateTimeField(auto_now_add=True, blank=True)
     updatedDate = models.DateTimeField(auto_now=True, blank=True)
