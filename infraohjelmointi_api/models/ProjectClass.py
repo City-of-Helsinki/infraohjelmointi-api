dd8eac879a531a691d50440bd567c76249e1fdfa
Uzair Ahmed <uzair.ahmed@Uzairs-MacBook-Pro-16.local>
2022-12-22 18:02:50 +0200

    Added related name to parent field to ease referencing reverse relationship

--- infraohjelmointi_api/models/ProjectClass.py
+++ infraohjelmointi_api/models/ProjectClass.py
@@ -5,6 +5,12 @@ from django.db import models
 class ProjectClass(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=150)
-    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="parentClass",
+    )
     createdDate = models.DateTimeField(auto_now_add=True, blank=True)
     updatedDate = models.DateTimeField(auto_now=True, blank=True)
