932b19f7b230d68066336c8eaeeaff055917b7d5
Uzair Ahmed <uzair.ahmed@Uzairs-MacBook-Pro-16.local>
2022-12-29 22:09:04 +0200

    ProjectLocation model and field added to Project

---
+++ infraohjelmointi_api/models/ProjectLocation.py
@@ -0,0 +1,25 @@
+import uuid
+from django.db import models
+
+
+class ProjectLocation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=150)
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="parentLocation",
+    )
+    createdDate = models.DateTimeField(auto_now_add=True, blank=True)
+    updatedDate = models.DateTimeField(auto_now=True, blank=True)
+
+    def path(self):
+        current = self
+        path = []
+        while current != None:
+            path.insert(0, current.name)
+            current = current.parent
+
+        return "/".join(path)
