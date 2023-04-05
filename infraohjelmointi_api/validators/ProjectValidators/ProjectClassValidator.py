4c774e8abd54af7c365c804e8631e44739ee069b
Uzair Ahmed <uzair.ahmed@Uzairs-MacBook-Pro-16.local>
2023-04-05 18:14:59 +0300

    Refactored projectClass/Location validaiton function into a util function

--- infraohjelmointi_api/validators/ProjectValidators/ProjectClassValidator.py
+++ infraohjelmointi_api/validators/ProjectValidators/ProjectClassValidator.py
@@ -1,32 +1,12 @@
 from rest_framework.exceptions import ValidationError
+from ..util.is_projectClass_projectLocation_valid import (
+    _is_projectClass_projectLocation_valid,
+)
 
 
 class ProjectClassValidator:
 
     requires_context = True
-
-    def _is_projectClass_projectLocation_valid(
-        self,
-        projectLocation,
-        projectClass,
-    ) -> bool:
-        if projectClass is None or projectLocation is None:
-            return True
-        if projectClass.parent is None or projectClass.parent.parent is None:
-            return True
-        if (
-            "suurpiiri" in projectClass.name.lower()
-            and len(projectClass.name.split()) == 2
-            and (
-                projectLocation.path.startswith(projectClass.name.split()[0])
-                or projectLocation.path.startswith(projectClass.name.split()[0][:-2])
-            )
-        ):
-            return True
-        elif "suurpiiri" not in projectClass.name.lower():
-            return True
-        else:
-            return False
 
     def __call__(self, allFields, serializer) -> None:
         projectClass = allFields.get("projectClass", None)
@@ -42,7 +22,7 @@ class ProjectClassValidator:
         ):
             projectLocation = project.projectLocation
 
-        if not self._is_projectClass_projectLocation_valid(
+        if not _is_projectClass_projectLocation_valid(
             projectLocation=projectLocation, projectClass=projectClass
         ):
             raise ValidationError(
--- infraohjelmointi_api/validators/ProjectValidators/ProjectLocationValidator.py
+++ infraohjelmointi_api/validators/ProjectValidators/ProjectLocationValidator.py
@@ -1,32 +1,12 @@
+from infraohjelmointi_api.validators.util.is_projectClass_projectLocation_valid import (
+    _is_projectClass_projectLocation_valid,
+)
 from rest_framework.exceptions import ValidationError
 
 
 class ProjectLocationValidator:
 
     requires_context = True
-
-    def _is_projectClass_projectLocation_valid(
-        self,
-        projectLocation,
-        projectClass,
-    ) -> bool:
-        if projectClass is None or projectLocation is None:
-            return True
-        if projectClass.parent is None or projectClass.parent.parent is None:
-            return True
-        if (
-            "suurpiiri" in projectClass.name.lower()
-            and len(projectClass.name.split()) == 2
-            and (
-                projectLocation.path.startswith(projectClass.name.split()[0])
-                or projectLocation.path.startswith(projectClass.name.split()[0][:-2])
-            )
-        ):
-            return True
-        elif "suurpiiri" not in projectClass.name.lower():
-            return True
-        else:
-            return False
 
     def __call__(self, allFields, serializer) -> None:
         projectLocation = allFields.get("projectLocation", None)
@@ -41,7 +21,7 @@ class ProjectLocationValidator:
             and project.projectClass is not None
         ):
             projectClass = project.projectClass
-        if not self._is_projectClass_projectLocation_valid(
+        if not _is_projectClass_projectLocation_valid(
             projectLocation=projectLocation, projectClass=projectClass
         ):
             raise ValidationError(
