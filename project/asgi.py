0d8a109690d6ab4ad8d4ff1379d2d2883edaf9e1
Kreshnik Gunga <kreshnik.gunga@robu.fi>
2023-05-16 11:29:26 +0300

    Refactor to use daphne server application to support server sent events

--- project/asgi.py
+++ project/asgi.py
@@ -1,12 +1,12 @@
 import os
-import django
 from django.core.asgi import get_asgi_application
 from django.urls import path, re_path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import django_eventstream
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+
 urlRouter = URLRouter(
     [
         path(
@@ -20,6 +20,5 @@ urlRouter = URLRouter(
 application = ProtocolTypeRouter(
     {
         "http": urlRouter,
-        "https": urlRouter,
     }
 )
