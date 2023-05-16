a6de3857f608ef63e5209891f4cbf6c3ab03a494
Kreshnik Gunga <kreshnik.gunga@robu.fi>
2023-05-16 20:38:10 +0300

    Implement workaround to support multi allowed hosts for event stream CORS

--- project/asgi.py
+++ project/asgi.py
@@ -3,15 +3,19 @@ from django.core.asgi import get_asgi_application
 from django.urls import path, re_path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import django_eventstream
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+
+from . import consumers
+
 
 urlRouter = URLRouter(
     [
         path(
             "events/",
-            AuthMiddlewareStack(URLRouter(django_eventstream.routing.urlpatterns)),
+            AuthMiddlewareStack(
+                URLRouter([path("", consumers.CostomEventConsumer.as_asgi())])
+            ),
             {"channels": ["finance", "project"]},
         ),
         re_path(r"", get_asgi_application()),
@@ -20,5 +24,6 @@ urlRouter = URLRouter(
 application = ProtocolTypeRouter(
     {
         "http": urlRouter,
+        "https": urlRouter,
     }
 )
