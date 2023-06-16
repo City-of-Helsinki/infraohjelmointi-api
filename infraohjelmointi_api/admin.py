3a06a9d0f9a9f102a694b28a65aa02192973dce3
Uzair Ahmed <uzair.ahmed@futurice.com>
2023-06-16 12:14:02 +0300

    Registered all remaining models to admin vie

--- infraohjelmointi_api/admin.py
+++ infraohjelmointi_api/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
-from .models import ProjectPhase
-from .models import Project
-from .models import ProjectArea
-from .models import Task
-from .models import BudgetItem
-from .models import ProjectSet
-from .models import ProjectPriority
-from .models import ProjectType
-from .models import Person
+
+from .models import *
 
 admin.site.register(Project)
 admin.site.register(ProjectArea)
@@ -18,6 +11,21 @@ admin.site.register(Task)
 admin.site.register(Person)
 admin.site.register(ProjectPhase)
 admin.site.register(ProjectPriority)
+admin.site.register(ConstructionPhase)
+admin.site.register(ConstructionPhaseDetail)
+admin.site.register(Note)
+admin.site.register(PlanningPhase)
+admin.site.register(ProjectCategory)
+admin.site.register(ProjectFinancial)
+admin.site.register(ProjectGroup)
+admin.site.register(ProjectHashTag)
+admin.site.register(ProjectLock)
+admin.site.register(ResponsibleZone)
+admin.site.register(TaskStatus)
+admin.site.register(ProjectQualityLevel)
+admin.site.register(ProjectClass)
+admin.site.register(ProjectRisk)
+admin.site.register(ProjectLocation)
 
 
 # Register your models here.
