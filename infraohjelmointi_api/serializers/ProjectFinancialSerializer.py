6019660c2b3d26d7680ad81ba56234c54656366d
Uzair Ahmed <uzair.ahmed@futurice.com>
2023-07-05 14:18:00 +0300

    Fixed tests after rebase, fixed validation issue, fixed serializers after rebase

--- infraohjelmointi_api/serializers/ProjectFinancialSerializer.py
+++ infraohjelmointi_api/serializers/ProjectFinancialSerializer.py
@@ -25,30 +25,3 @@ class ProjectFinancialSerializer(serializers.ModelSerializer):
         rep = super().to_representation(instance)
         rep.pop("project")
         return rep
-
-    @override
-    def update(self, instance, validated_data):
-        # Check if project is locked and any locked fields are not being updated
-        if hasattr(instance.project, "lock"):
-            lockedFields = [
-                "budgetProposalCurrentYearPlus0",
-                "budgetProposalCurrentYearPlus1",
-                "budgetProposalCurrentYearPlus2",
-                "preliminaryCurrentYearPlus3",
-                "preliminaryCurrentYearPlus4",
-                "preliminaryCurrentYearPlus5",
-                "preliminaryCurrentYearPlus6",
-                "preliminaryCurrentYearPlus7",
-                "preliminaryCurrentYearPlus8",
-                "preliminaryCurrentYearPlus9",
-                "preliminaryCurrentYearPlus10",
-            ]
-            for field in lockedFields:
-                if validated_data.get(field, None) is not None:
-                    raise serializers.ValidationError(
-                        "The field {} cannot be modified when the project is locked".format(
-                            field
-                        )
-                    )
-
-        return super(ProjectFinancialSerializer, self).update(instance, validated_data)
--- infraohjelmointi_api/serializers/ProjectWithFinancesSerializer.py
+++ infraohjelmointi_api/serializers/ProjectWithFinancesSerializer.py
@@ -17,16 +17,32 @@ class ProjectWithFinancesSerializer(serializers.ModelSerializer):
         If no year is passed to the serializer using either the project id or finance_year as key
         the current year is used as the default.
         """
+
         year = self.context.get(
             str(project.id), self.context.get("finance_year", date.today().year)
         )
         if year is None:
             year = date.today().year
-        queryset, _ = ProjectFinancialService.get_or_create(
-            project_id=project.id, year=year
+        year = int(year)
+        yearToFieldMapping = (
+            ProjectFinancialService.get_year_to_financial_field_names_mapping(
+                start_year=year
+            )
         )
+        queryset = ProjectFinancialService.find_by_project_id_and_year_range(
+            project_id=project.id, year_range=range(year, year + 11)
+        )
+        allFinances = ProjectFinancialSerializer(queryset, many=True).data
+        serializedFinances = {"year": year}
+        for finance in allFinances:
+            serializedFinances[yearToFieldMapping[finance["year"]]] = finance["value"]
+            # pop out already mapped keys
+            yearToFieldMapping.pop(finance["year"])
+        # remaining year keys which had no data in DB
+        for yearKey in yearToFieldMapping.keys():
+            serializedFinances[yearToFieldMapping[yearKey]] = "0.00"
 
-        return ProjectFinancialSerializer(queryset, many=False).data
+        return serializedFinances
 
     class Meta(BaseMeta):
         model = Project
