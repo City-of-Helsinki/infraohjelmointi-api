66826dd7f93d711e81bb2afafc60a83aba3172ac
Uzair Ahmed <uzair.ahmed@futurice.com>
2023-08-03 19:25:50 +0300

    Added classRelation field validator to ClassFinancialSerializer

--- infraohjelmointi_api/serializers/ClassFinancialSerializer.py
+++ infraohjelmointi_api/serializers/ClassFinancialSerializer.py
@@ -1,12 +1,19 @@
-from infraohjelmointi_api.models import ClassFinancial
+from infraohjelmointi_api.models import ClassFinancial, ProjectClass
 from infraohjelmointi_api.serializers import BaseMeta
-
+from infraohjelmointi_api.validators.ClassFinancialValidators import (
+    ClassRelationFieldValidator,
+)
 from rest_framework import serializers
-from overrides import override
 from rest_framework.validators import UniqueTogetherValidator
 
 
 class ClassFinancialSerializer(serializers.ModelSerializer):
+    classRelation = serializers.PrimaryKeyRelatedField(
+        many=False,
+        validators=[ClassRelationFieldValidator()],
+        queryset=ProjectClass.objects.all(),
+    )
+
     class Meta(BaseMeta):
         model = ClassFinancial
         exclude = ["createdDate", "updatedDate", "id"]
