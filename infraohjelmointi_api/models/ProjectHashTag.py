e3bd17b117637e4ef894277316c3ca593eee3f6d
Uzair Ahmed <uzair.ahmed@futurice.com>
2023-08-18 12:48:36 +0300

    added read_only property usageCount to project hashtags response, added new archived boolean field to hashtags

--- infraohjelmointi_api/models/ProjectHashTag.py
+++ infraohjelmointi_api/models/ProjectHashTag.py
@@ -5,5 +5,6 @@ from django.db import models
 class ProjectHashTag(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     value = models.CharField(max_length=100)
+    archived = models.BooleanField(default=False, null=False, blank=False)
     createdDate = models.DateTimeField(auto_now_add=True, blank=True)
     updatedDate = models.DateTimeField(auto_now=True, blank=True)
