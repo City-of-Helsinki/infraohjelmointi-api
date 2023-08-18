f78b294bd2770b681a339f50cb4213d81bef05cc
Uzair Ahmed <uzair.ahmed@futurice.com>
2023-08-18 12:48:36 +0300

    added read_only property usageCount to project hashtags response, added new archived boolean field to hashtags

--- infraohjelmointi_api/serializers/ProjectHashtagSerializer.py
+++ infraohjelmointi_api/serializers/ProjectHashtagSerializer.py
@@ -1,7 +1,14 @@
 from infraohjelmointi_api.models import ProjectHashTag
 from infraohjelmointi_api.serializers import BaseMeta, DynamicFieldsModelSerializer
+from rest_framework import serializers
 
 
 class ProjectHashtagSerializer(DynamicFieldsModelSerializer):
+    usageCount = serializers.SerializerMethodField(read_only=True)
+
     class Meta(BaseMeta):
+        exclude = ["updatedDate"]
         model = ProjectHashTag
+
+    def get_usageCount(self, hashTag: ProjectHashTag):
+        return hashTag.relatedProject.count()
