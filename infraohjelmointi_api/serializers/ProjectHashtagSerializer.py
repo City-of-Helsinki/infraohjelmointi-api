2ac13bcd378686319d9a913ee93d5800f12f03ce
Uzair Ahmed <uzair.ahmed@futurice.com>
2023-08-22 09:11:10 +0300

    Regenerated migration file with unique=True, added unique validation to ProjectHashtag

--- infraohjelmointi_api/serializers/ProjectHashtagSerializer.py
+++ infraohjelmointi_api/serializers/ProjectHashtagSerializer.py
@@ -1,10 +1,19 @@
 from infraohjelmointi_api.models import ProjectHashTag
 from infraohjelmointi_api.serializers import BaseMeta, DynamicFieldsModelSerializer
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 
 class ProjectHashtagSerializer(DynamicFieldsModelSerializer):
     usageCount = serializers.SerializerMethodField(read_only=True)
+    value = serializers.CharField(
+        validators=[
+            UniqueValidator(
+                queryset=ProjectHashTag.objects.all(),
+                message=("HashTag with this name already exists"),
+            )
+        ]
+    )
 
     class Meta(BaseMeta):
         exclude = ["updatedDate"]
