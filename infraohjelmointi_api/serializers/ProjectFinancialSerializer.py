65e4e0a5137d83b962fbde4acc45585ffa7bd9d8
Uzair Ahmed <uzair.ahmed@futurice.com>
2023-08-29 13:13:19 +0300

    overriden create and update methods of ProjectFinancialSerializer to populate frame view projec tfinancials if they don't exists and planning/coordinator project financials are being updated/made

--- infraohjelmointi_api/serializers/ProjectFinancialSerializer.py
+++ infraohjelmointi_api/serializers/ProjectFinancialSerializer.py
@@ -21,6 +21,45 @@ class ProjectFinancialSerializer(serializers.ModelSerializer):
         ]
 
     @override
+    def create(self, validated_data):
+        # Check if a non frame view project financial is created and the related frameview financial does not exist
+        if validated_data.get("forframeView", False) == False:
+            if not ProjectFinancial.objects.filter(
+                project=validated_data.get("project"),
+                year=validated_data.get("year"),
+                forFrameView=True,
+            ).exists():
+                ProjectFinancial.objects.create(
+                    project=validated_data.get("project"),
+                    year=validated_data.get("year"),
+                    forFrameView=True,
+                    value=validated_data.get("value", 0),
+                )
+
+        projectFinancialObject = super(ProjectFinancialSerializer, self).create(
+            validated_data
+        )
+
+        return projectFinancialObject
+
+    @override
+    def update(self, instance: ProjectFinancial, validated_data):
+        # Check if a non frame view project financial is updated and the related frameview financial does not exist
+        if instance.forFrameView == False:
+            if not ProjectFinancial.objects.filter(
+                project=instance.project,
+                year=instance.year,
+                forFrameView=True,
+            ).exists():
+                ProjectFinancial.objects.create(
+                    project=instance.project,
+                    year=instance.year,
+                    forFrameView=True,
+                    value=validated_data.get("value", 0),
+                )
+        return super(ProjectFinancialSerializer, self).update(instance, validated_data)
+
+    @override
     def to_representation(self, instance):
         rep = super().to_representation(instance)
         rep.pop("project")
