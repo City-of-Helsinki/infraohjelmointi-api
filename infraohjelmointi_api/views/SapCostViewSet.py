3e0ce59523f9f4f7347bec967850efb692feded3
Uzair Ahmed <uzair.ahmed@futurice.com>
2023-09-25 09:29:01 +0300

    Added custom action to sapcost viewset for fetching sap costs by year

--- infraohjelmointi_api/views/SapCostViewSet.py
+++ infraohjelmointi_api/views/SapCostViewSet.py
@@ -4,13 +4,14 @@ from rest_framework.decorators import action
 import logging
 from overrides import override
 from rest_framework.response import Response
+from infraohjelmointi_api.services.SapCostService import SapCostService
 
 logger = logging.getLogger("infraohjelmointi_api")
 
 
 class SapCostViewSet(BaseViewSet):
     """
-    API endpoint that allows Tasks to be viewed or edited.
+    API endpoint that allows Sap Cost to be viewed or edited.
     """
 
     serializer_class = SapCostSerializer
@@ -19,3 +20,34 @@ class SapCostViewSet(BaseViewSet):
     @override
     def list(self, request, *args, **kwargs):
         return self.retrieve(request=request, args=args, kwargs=kwargs)
+
+    @action(
+        methods=["get"],
+        detail=False,
+        url_path=r"(?P<year>[0-9]{4})",
+    )
+    def get_sap_cost_by_year(self, request, year):
+        """
+        Custom action to get sap cost by year
+
+            URL Parameters
+            ----------
+
+            year : int
+
+            Usage
+            ----------
+
+            sap-costs/<year>/
+
+            Returns
+            -------
+
+            JSON
+                List of sap costs from the year provided
+        """
+
+        qs = SapCostService.get_by_year(int(year))
+        serializer = self.get_serializer(qs, many=True)
+
+        return Response(serializer.data)
