3e6cbdf6fc93ad3b96e3c6da0d749ed201408ba6
Kreshnik Gunga <kreshnik.gunga@robu.fi>
2023-09-26 14:56:25 +0300

    Fix Sonar issues

--- infraohjelmointi_api/management/commands/util/hierarchy.py
+++ infraohjelmointi_api/management/commands/util/hierarchy.py
@@ -625,7 +625,9 @@ def buildHierarchiesAndProjects(
                 ProjectLocationService.get_or_create(
                     # keepe number in front of division
                     name="{} {}".format(
-                        re.sub("[^\d.]+\s*$", "", str(cell.value).strip()).strip(),
+                        re.sub(
+                            "[^\d.]{0,100}\s*$", "", str(cell.value).strip()
+                        ).strip(),
                         name,
                     ),
                     parent=location_stack[-1],
