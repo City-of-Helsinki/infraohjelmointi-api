eb74d67961624488eea0858b2debf878a36233a4
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2023-09-27 07:27:45 +0300

    Logic for setting new notes

--- infraohjelmointi_api/models/CoordinatorNote.py
+++ infraohjelmointi_api/models/CoordinatorNote.py
@@ -1,7 +1,5 @@
 import uuid
 from django.db import models
-from .Person import Person
-from .Project import Project
 
 class CoordinatorNote(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -9,11 +7,12 @@ class CoordinatorNote(models.Model):
     coordinatorNote = models.TextField(blank=True, null=False, default="")
 
     planningClass = models.CharField(max_length=150)
-    planningClassId = models.ForeignKey("Project", on_delete=models.DO_NOTHING)
+    planningClassId = models.ForeignKey("ProjectClass", on_delete=models.DO_NOTHING)
 
-    updatedById = models.ForeignKey("Person", on_delete=models.DO_NOTHING, null=False, blank=False)
+    updatedById = models.ForeignKey("User", on_delete=models.DO_NOTHING, null=False, blank=False)
     updatedByFirstName = models.CharField(max_length=50)
     updatedByLastName = models.CharField(max_length=50)
 
     createdDate = models.DateTimeField(auto_now_add=True, blank=True)
     updatedDate = models.DateTimeField(auto_now=True, blank=True)
+    
