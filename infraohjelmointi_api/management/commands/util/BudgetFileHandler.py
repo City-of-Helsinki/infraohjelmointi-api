f2d48011db4c21f1c1906a9b4fcd5e2337a3cdf4
Milla Eerola <77616473+millaeerola@users.noreply.github.com>
2023-11-07 14:52:31 +0200

    Release 7.11.2023 (#90)
    * skip column if length too short
    
    * Ignore new excel files
    
    * ignore zip files
    
    * skip excel row if empty
    
    * fix models that gave errors and change one log message to be info
    
    * don't save value if empty
    
    * initial commit
    
    * swich level where other class color is handled
    
    * remove unused code
    
    * remove duplicate keys
    
    * read projectwise number correctly
    
    * make a migration for modified project model
    
    ---------
    
    Co-authored-by: Markus Palomäki <markus.palomaki.external@zalando.de>
    Co-authored-by: Markus Palomäki <markus.palomaki@futurice.com>

--- infraohjelmointi_api/management/commands/util/BudgetFileHandler.py
+++ infraohjelmointi_api/management/commands/util/BudgetFileHandler.py
@@ -32,8 +32,8 @@ class BudgetFileHandler(IExcelFileHandler):
         self.current_budget_year = datetime.date.today().year
 
     def proceed_with_file(self, excel_path):
-        logger.error(
-            "\n\nReading project data from budget file {}\n".format(excel_path)
+        logger.info(
+            "\nReading project data from budget file {}\n".format(excel_path)
         )
 
         wb = load_workbook(excel_path, data_only=True, read_only=True)
@@ -148,6 +148,7 @@ class BudgetFileHandler(IExcelFileHandler):
         preliminaryCurrentYearPlus9 = row[28].value
 
         budget_list = [
+            0,
             budgetProposalCurrentYearPlus0 or 0,
             budgetProposalCurrentYearPlus1 or 0,
             budgetProposalCurrentYearPlus2 or 0,
@@ -163,7 +164,13 @@ class BudgetFileHandler(IExcelFileHandler):
         budget_sum = sum(budget_list)
 
         notes = str(row[29].value).strip()
-        pwNumber = str(row[30].value).strip().lower()
+
+        # some rows might not have pw number
+        # with this we skip the index out of range -error
+        
+        pwNumber = None
+        if not len(row) < 31:
+            pwNumber = str(row[30].value).strip().lower()
 
         try:
             project = ProjectService.get(
