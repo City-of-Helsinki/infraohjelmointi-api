a7f67ae3c5da6a48089cc5badfc4d72fdf04d15c
Milla Eerola <milla.eerola@futurice.com>
2024-01-10 11:38:46 +0200

    fix namings

--- infraohjelmointi_api/management/commands/util/locationFileHandler.py
+++ infraohjelmointi_api/management/commands/util/locationFileHandler.py
@@ -4,19 +4,19 @@ from infraohjelmointi_api.services import ProjectDistrictService
 def addLocations(rows):
     for row in rows[1:]:
         district = row[1].value
-        subDistrict = row[2].value
-        subSubDistrict = row[3].value
+        division = row[2].value
+        subDivision = row[3].value
 
         # creating the path of the division and getting it's parent path
         # if it has that
         path = row[1].value
         subClassParentPath = None
         subSubClassParentPath = None
-        if district != subDistrict:
-            path += " / " + subDistrict
+        if district != division:
+            path += " / " + division
             subClassParentPath = path
-            if subDistrict != subSubDistrict:
-                path += " / " + subSubDistrict
+            if division != subDivision:
+                path += " / " + subDivision
                 subSubClassParentPath = path
 
 
@@ -28,18 +28,18 @@ def addLocations(rows):
             )[0]
         print(district.name)
         if subClassParentPath:
-            subDistrict = ProjectDistrictService.get_or_create(
-                name=subDistrict, 
+            division = ProjectDistrictService.get_or_create(
+                name=division, 
                 parent=district, 
                 path=subClassParentPath,
-                level="subDistrict",
+                level="division",
                 )[0]
-            print(subDistrict.name)
+            print(division.name)
             if subSubClassParentPath:
                 subsubDistrict = ProjectDistrictService.get_or_create(
-                    name=subSubDistrict, 
-                    parent=subDistrict, 
+                    name=subDivision, 
+                    parent=division, 
                     path=path,
-                    level="subSubDistrict",
+                    level="subDivision",
                     )[0]
                 print(subsubDistrict.name)
