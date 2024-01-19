e3fed775e2bc1a65707973152c951ba33e619cfb
Markus Palomäki <markus.palomaki@futurice.com>
2024-01-19 13:31:42 +0200

    change comments

--- update-districts.sql
+++ update-districts.sql
@@ -1,24 +1,14 @@
---hankkeiden suurpiirit
---jos projectLocation_id tyhjä, älä tee mitään
---jos projectLocation_id ei ole tyhjä:
---projectLocation taulusta haetaan ID:llä mikä se on
---otetaan path
---haetaan pathilla projectdistrictistä == path
---projectdistrictistä id, syötetään projectDistrict_id
+-- Update missing projectDistrict_id with infraohjelmointi_api_projectdistrict.id
+-- This need to be done, because original Excel files uses old location information
+
 UPDATE infraohjelmointi_api_project 
 SET "projectDistrict_id" = infraohjelmointi_api_projectdistrict.id
 FROM infraohjelmointi_api_projectdistrict, infraohjelmointi_api_projectlocation
 WHERE infraohjelmointi_api_project."projectLocation_id" = infraohjelmointi_api_projectlocation.id
 AND infraohjelmointi_api_projectlocation.path = infraohjelmointi_api_projectdistrict.path;
 
+-- Update missing location_id with infraohjelmointi_api_projectdistrict.id
 
---ryhmien suurpiirit projectgroup
---jos locationRelation_id tyhjä, älä tee mitään
---jos locationRelation_id ei ole tyhjä:
---projectLocation taulusta haetaan ID:llä mikä se on
---otetaan path
---haetaan pathilla projectdistrictistä == path
---projectdistrictistä id, syötetään projectgroupiin location_id
 UPDATE infraohjelmointi_api_projectgroup
 SET "location_id" = infraohjelmointi_api_projectdistrict.id
 FROM infraohjelmointi_api_projectdistrict, infraohjelmointi_api_projectlocation
