aae8a085d4bd0928216a86a8df8f471e4f9af842
Saija Seppä <78361679+saijaseppa@users.noreply.github.com>
2024-02-05 10:32:25 +0200

    Add logger

--- infraohjelmointi_api/views/ProjectViewSet.py
+++ infraohjelmointi_api/views/ProjectViewSet.py
@@ -1,4 +1,5 @@
 from datetime import date
+import logging
 from django_filters.rest_framework import DjangoFilterBackend
 import django_filters
 from infraohjelmointi_api.serializers import (
@@ -38,6 +39,9 @@ from rest_framework import status
 from itertools import chain
 from django.db.models import Count, Case, When, Q
 from django.db.models.signals import post_save
+
+logger = logging.getLogger("infraohjelmointi_api")
+
 
 class ProjectFilter(django_filters.FilterSet):
     hashtag = django_filters.ModelMultipleChoiceFilter(
