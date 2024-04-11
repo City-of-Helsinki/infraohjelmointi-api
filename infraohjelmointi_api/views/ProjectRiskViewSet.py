d6dc04b1bccd73374339a23cba45c868cd6e7fcf
Milla Eerola <milla.eerola@futurice.com>
2024-04-11 14:50:21 +0300

    refactor: remove cache

--- infraohjelmointi_api/views/ProjectRiskViewSet.py
+++ infraohjelmointi_api/views/ProjectRiskViewSet.py
@@ -1,7 +1,5 @@
 from .BaseViewSet import BaseViewSet
 from infraohjelmointi_api.serializers.ProjectRiskSerializer import ProjectRiskSerializer
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
 
 
 class ProjectRiskViewSet(BaseViewSet):
@@ -10,7 +8,3 @@ class ProjectRiskViewSet(BaseViewSet):
     """
 
     serializer_class = ProjectRiskSerializer
-
-    @method_decorator(cache_page(60 * 60 * 24))
-    def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
