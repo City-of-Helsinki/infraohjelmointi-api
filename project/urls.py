0e38e277d00c97c4a7b2035b90aa8c620a0b7087
Markus Palomäki <markus.palomaki@futurice.com>
2024-04-23 16:27:06 +0300

    refactor: remove session auth and update description

--- project/urls.py
+++ project/urls.py
@@ -136,11 +136,12 @@ apirouter.register(
 schema_view = get_schema_view(
     openapi.Info(
         title="Infraohjelmointi API",
-        default_version='v1',
+        description="API documentation for Infrahankkeiden ohjelmointi",
+        default_version="v1"
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-    patterns=[path(r"^/api/", include((apirouter.urls, "api"), namespace="v1"))],
+    patterns=[path("", include((apirouter.urls, "api")))],
 )
 
 urlpatterns = [
@@ -153,5 +154,4 @@ urlpatterns = [
     path("pysocial/", include("social_django.urls", namespace="social")),
     path("helauth/", include("helusers.urls")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
