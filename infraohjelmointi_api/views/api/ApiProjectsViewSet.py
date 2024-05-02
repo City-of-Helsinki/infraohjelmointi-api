933fde7678c2813cd820824607f677f748b1a9e4
Markus Palomäki <markus.palomaki@futurice.com>
2024-05-02 11:16:52 +0300

    refactor: separate endpoints as own files and move Swagger UI under /api/

--- infraohjelmointi_api/views/api/ApiProjectsViewSet.py
+++ infraohjelmointi_api/views/api/ApiProjectsViewSet.py
@@ -1,10 +1,12 @@
-from .BaseViewSet import BaseViewSet
+from ..BaseViewSet import BaseViewSet
 from django.utils.decorators import method_decorator
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from infraohjelmointi_api.models import Project
 from infraohjelmointi_api.serializers import ProjectGetSerializer
+import uuid
+from rest_framework import status
 
 from drf_yasg.utils import swagger_auto_schema
 
@@ -23,7 +25,7 @@ class ApiProjectsViewSet(BaseViewSet):
 
     queryset = Project.objects.all()
     serializer_class = ProjectGetSerializer
-    
+
 
     @swagger_auto_schema(
             operation_description = """
@@ -33,8 +35,13 @@ class ApiProjectsViewSet(BaseViewSet):
             """,
             )
     def retrieve(self, request, pk=None):
-        queryset = self.get_queryset()
-        obj = queryset.get(pk=pk)
-        serializer = self.get_serializer(obj)
-        return Response(serializer.data)
-    
+        try:
+            uuid.UUID(str(pk))
+            queryset = self.get_queryset()
+            obj = queryset.get(pk=pk)
+            serializer = self.get_serializer(obj)
+            return Response(serializer.data)
+        except:
+            return Response(
+                data={"message": "Not found"}, status=status.HTTP_404_NOT_FOUND
+            )
--- project/urls.py
+++ project/urls.py
@@ -149,5 +149,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("pysocial/", include("social_django.urls", namespace="social")),
     path("helauth/", include("helusers.urls")),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
