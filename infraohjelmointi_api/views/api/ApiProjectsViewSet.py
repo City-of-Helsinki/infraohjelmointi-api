ca332928f1e8c2864ab05a5ed4646d75ecc26d64
Markus Palomäki <markus.palomaki@futurice.com>
2024-05-02 16:27:27 +0300

    feat: add query parameter for Project query to fetch all projects by class id

--- infraohjelmointi_api/views/api/ApiProjectsViewSet.py
+++ infraohjelmointi_api/views/api/ApiProjectsViewSet.py
@@ -3,19 +3,28 @@ from django.utils.decorators import method_decorator
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from infraohjelmointi_api.models import Project
+from infraohjelmointi_api.models import Project, ProjectClass
 from infraohjelmointi_api.serializers import ProjectGetSerializer
 import uuid
 from rest_framework import status
 
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+
+class_parameter = openapi.Parameter(
+    'class',
+    in_=openapi.IN_QUERY,
+    description='Get all projects by class ID',
+    type=openapi.TYPE_STRING
+)
 
 @method_decorator(name='list', decorator=swagger_auto_schema(
     operation_description="""
     `GET /api/projects/`
 
     Get all projects.
-    """
+    """,
+    manual_parameters=[class_parameter]
 ))
 class ApiProjectsViewSet(BaseViewSet):
     http_method_names = ['get']
@@ -45,3 +54,13 @@ class ApiProjectsViewSet(BaseViewSet):
             return Response(
                 data={"message": "Not found"}, status=status.HTTP_404_NOT_FOUND
             )
+
+
+    def list(self, request, *args, **kwargs):
+        project_class_id = self.request.query_params.get('class')
+        queryset = self.queryset
+        if project_class_id is not None:
+            queryset = queryset.filter(projectClass__id=uuid.UUID(project_class_id))
+        self.queryset = queryset
+        serializer = self.get_serializer(self.queryset, many=True)
+        return Response(serializer.data)
