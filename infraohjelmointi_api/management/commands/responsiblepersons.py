d094133aacd1e7ea351858263d547f613d5720e4
Markus Palomäki <markus.palomaki@futurice.com>
2024-10-09 14:56:02 +0300

    refactor: rewrite person adding logic that checks and adds a new person

--- infraohjelmointi_api/management/commands/responsiblepersons.py
+++ infraohjelmointi_api/management/commands/responsiblepersons.py
@@ -69,6 +69,7 @@ class Command(BaseCommand):
         wb = load_workbook(excelPath, data_only=True, read_only=True)
         rows = list(wb.worksheets[0].rows)
         incorrect_emails = []
+        duplicates = []
 
         for row in rows:
             if len(row) < 2:
@@ -87,24 +88,84 @@ class Command(BaseCommand):
                 incorrect_emails.append((firstname, lastname, email))
                 continue
 
-            person, _ = PersonService.get_or_create_by_name_and_email(
-                firstName=firstname, lastName=lastname, email=email
+            # Get person by email. Create new person if name or email are not found.
+            # In case person was found without an email, we update the person.
+            # In case multiple person with the same name, we do nothing but show these as a result.
+            person_by_email = PersonService.get_by_email(
+                email=email
             )
 
-            if person:
-                logger.info(
-                    "Person added: {} {}, email '{}' ({})".format(
-                        person.firstName, person.lastName, person.email, person.id
+            if person_by_email is None:
+                person = PersonService.get_by_name(
+                    firstName=firstname,
+                    lastName=lastname
+                )
+
+                if isinstance(person, list):
+                    # When multiple names found, PersonService returns a list.
+                    # In this case, do nothing and show the duplicate at the end.
+                    duplicates.append((firstname,lastname,email))
+
+                    logger.warning(
+                        "Duplicate found: {} {}, '{}'".format(
+                            firstname, lastname, email
+                        )
+                    )
+
+                    continue
+
+                if person is None:
+                    # Create new person if not found by email and name
+                    person = PersonService.create_by_name(
+                        firstName=firstname,
+                        lastName=lastname,
+                        email=email
+                    )
+
+                    logger.info(
+                        "Person added: '{}', {} {} ({})".format(
+                            person.email, person.firstName, person.lastName, person.id
+                    )
+                )
+
+                elif not person.email:
+                    # Update missing email address for found name when email is not set
+                    person.email = email
+                    person.save()
+
+                    logger.info(
+                        "Email updated: '{}', {} {} ({})".format(
+                            person.email, person.firstName, person.lastName, person.id
+                        )
+                )
+            else:
+                logger.warning(
+                    "Email found, skip. Found data: [{}, {}, {}, {}]".format(
+                        person_by_email.id,
+                        person_by_email.firstName,
+                        person_by_email.lastName,
+                        person_by_email.email
                     )
                 )
 
         # Print list of incorrect email data
         if len(incorrect_emails) > 0:
-            printable_list = "Error with following data:"
+            printable_list = "Error with following data that were not added:"
 
             for incorrect_person in incorrect_emails:
                 printable_list += "\n{} {}, email: '{}'".format(
                     incorrect_person[0], incorrect_person[1], incorrect_person[2]
                 )
 
-            logger.error(printable_list)
+            logger.warning(printable_list)
+
+        # Print list of duplicates
+        if len(duplicates) > 0:
+            printable_list = "Found name duplicates (firstname, lastname) that were not added:"
+
+            for duplicate in duplicates:
+                printable_list += "\n{} {}, email: '{}".format(
+                    duplicate[0], duplicate[1], duplicate[2]
+                )
+
+            logger.warning(printable_list)
