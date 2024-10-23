9a62f066b0e9be4986ba055223e02cec8c4d5d76
Milla Eerola <milla.eerola@futurice.com>
2024-10-23 13:48:46 +0300

    feat: IO353 fix migration

--- infraohjelmointi_api/serializers/AppStateValueSerializer.py
+++ infraohjelmointi_api/serializers/AppStateValueSerializer.py
@@ -1,8 +1,8 @@
-from infraohjelmointi_api.models import AppStateValueModel
+from infraohjelmointi_api.models import AppStateValue
 from infraohjelmointi_api.serializers import BaseMeta
 from rest_framework import serializers
 
 
 class AppStateValueSerializer(serializers.ModelSerializer):
     class Meta(BaseMeta):
-        model = AppStateValueModel
+        model = AppStateValue
