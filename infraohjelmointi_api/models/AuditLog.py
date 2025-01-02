5316182c2651382d18c82d600d1beb039419c6b9
Milla Eerola <milla.eerola@futurice.com>
2025-01-02 12:33:29 +0200

    feat: IO-147 change AuditLog model fields to not be editable

--- infraohjelmointi_api/models/AuditLog.py
+++ infraohjelmointi_api/models/AuditLog.py
@@ -22,23 +22,23 @@ class AuditLog(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     actor = models.ForeignKey(
-        User, on_delete=models.CASCADE, null=False, blank=False
+        User, on_delete=models.CASCADE, null=False, blank=False, editable=False
     )
-    operation = models.CharField(max_length=20, choices=OPERATION_CHOICES, blank=False, null=False) # what was done e.g. update or delete
-    log_level = models.CharField(max_length=20, choices=LOG_LEVEL_CHOICES, blank=False, null=False) # INFO / ERROR
-    origin = models.CharField(max_length=100, blank=False, null=False) # only infrahankkeiden_ohjelmointi for now
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, blank=False, null=False)  # SUCCESS / FAILURE
+    operation = models.CharField(max_length=20, choices=OPERATION_CHOICES, blank=False, null=False, editable=False) # what was done e.g. update or delete
+    log_level = models.CharField(max_length=20, choices=LOG_LEVEL_CHOICES, blank=False, null=False, editable=False) # INFO / ERROR
+    origin = models.CharField(max_length=100, blank=False, null=False, editable=False) # only infrahankkeiden_ohjelmointi for now
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, blank=False, null=False, editable=False)  # SUCCESS / FAILURE
     project = models.ForeignKey(
-        Project, on_delete=models.DO_NOTHING, null=True, blank=True
+        Project, on_delete=models.DO_NOTHING, null=True, blank=True, editable=False
     )
     project_group = models.ForeignKey(
-        ProjectGroup, on_delete=models.DO_NOTHING, null=True, blank=True
+        ProjectGroup, on_delete=models.DO_NOTHING, null=True, blank=True, editable=False
     )
     project_class = models.ForeignKey(
-        ProjectClass, on_delete=models.DO_NOTHING, null=True, blank=True
+        ProjectClass, on_delete=models.DO_NOTHING, null=True, blank=True, editable=False
     )
-    old_values = models.JSONField(null=True, blank=True)
-    new_values = models.JSONField(null=True, blank=True)
-    endpoint = models.CharField(max_length=200, blank=False, null=False) #endpoint that was called to do the operation
-    createdDate = models.DateTimeField(auto_now_add=True, blank=True)
-    updatedDate = models.DateTimeField(auto_now=True, blank=True)
+    old_values = models.JSONField(null=True, blank=True, editable=False)
+    new_values = models.JSONField(null=True, blank=True, editable=False)
+    endpoint = models.CharField(max_length=200, blank=False, null=False, editable=False) #endpoint that was called to do the operation
+    createdDate = models.DateTimeField(auto_now_add=True, blank=True, editable=False)
+    updatedDate = models.DateTimeField(auto_now=True, blank=True, editable=False)
