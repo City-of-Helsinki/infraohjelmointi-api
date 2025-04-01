89321253d9d3a747fef45e263f432a494320dfd2
miikko <miikka.oksanen@pp1.inet.fi>
2025-04-01 11:16:44 +0300

    refactor: HKISD-279 make some PowerBI endpoints faster

--- infraohjelmointi_api/views/api/ApiClassesViewSet.py
+++ infraohjelmointi_api/views/api/ApiClassesViewSet.py
@@ -1,9 +1,15 @@
+from datetime import date
+from collections import defaultdict
 from ..BaseViewSet import BaseViewSet
 from django.utils.decorators import method_decorator
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from infraohjelmointi_api.serializers import ProjectClassSerializer
+from infraohjelmointi_api.models import (
+    LocationFinancial, ClassFinancial
+)
 from django.http import StreamingHttpResponse
+from django.db.models import F
 from .utils import generate_response, generate_streaming_response, send_logger_api_generate_data_start
 
 from drf_yasg.utils import swagger_auto_schema
@@ -40,9 +46,26 @@ class ApiClassesViewSet(BaseViewSet):
     def list(self, request, *args, **kwargs):
         send_logger_api_generate_data_start(request.user.id, request.path)
         queryset = self.get_queryset()
+
+        year = date.today().year      
+        class_financials = ClassFinancial.objects.filter(
+            year__in=range(year, year+11)
+        ).annotate(relation=F("classRelation")).values("year", "relation", "frameBudget")
+        location_financials = LocationFinancial.objects.filter(
+            year__in=range(year, year+11)
+        ).annotate(relation=F("locationRelation")).values("year", "relation", "frameBudget")
+        financials = class_financials.union(location_financials)
+        frame_budgets = defaultdict(lambda: 0)
+        for f in financials:
+            frame_budgets[f"{f['year']}-{f['relation']}"] = f["frameBudget"]
+
         return StreamingHttpResponse(
             generate_streaming_response(
-                queryset, self.serializer_class, request.user.id, request.path
+                queryset,
+                self.serializer_class,
+                request.user.id,
+                request.path,
+                serializer_context={"frame_budgets": frame_budgets}
             ),
             content_type="application/json",
         )
