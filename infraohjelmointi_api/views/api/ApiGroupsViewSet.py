7d9d1f4d0d923add50b4365ba760591c9983a069
Markus Palomäki <14055798+markspl@users.noreply.github.com>
2025-04-03 15:38:29 +0300

    Revert "Release 27.3.2025"

--- infraohjelmointi_api/views/api/ApiGroupsViewSet.py
+++ infraohjelmointi_api/views/api/ApiGroupsViewSet.py
@@ -3,52 +3,45 @@ from django.utils.decorators import method_decorator
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from infraohjelmointi_api.models import ProjectGroup
 from infraohjelmointi_api.serializers import ProjectGroupSerializer
+import uuid
 from rest_framework import status
-from django.http import StreamingHttpResponse
-from .utils import generate_response, generate_streaming_response
 
 from drf_yasg.utils import swagger_auto_schema
 
-
-@method_decorator(
-    name="list",
-    decorator=swagger_auto_schema(
-        operation_description="""
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_description="""
     `GET /api/groups/`
 
     Get all projects.
     """
-    ),
-)
+))
 class ApiGroupsViewSet(BaseViewSet):
-    http_method_names = ["get"]
+    http_method_names = ['get']
 
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    queryset = ProjectGroup.objects.all()
     serializer_class = ProjectGroupSerializer
 
+
     @swagger_auto_schema(
-        operation_description="""
+            operation_description = """
             `GET /api/groups/{id}`
 
-            Get specific project group data.
+            Get all project groups.
             """,
-    )
+            )
     def retrieve(self, request, pk=None):
         try:
-            return generate_response(self, request.user.id, pk, request.path)
+            uuid.UUID(str(pk))
+            queryset = self.get_queryset()
+            obj = queryset.get(pk=pk)
+            serializer = self.get_serializer(obj)
+            return Response(serializer.data)
         except Exception:
             return Response(
                 data={"message": "Not found"}, status=status.HTTP_404_NOT_FOUND
             )
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        return StreamingHttpResponse(
-            generate_streaming_response(
-                queryset, self.serializer_class, request.user.id, request.path
-            ),
-            content_type="application/json",
-        )
--- infraohjelmointi_api/views/api/ApiLocationsViewSet.py
+++ infraohjelmointi_api/views/api/ApiLocationsViewSet.py
@@ -3,58 +3,48 @@ from django.utils.decorators import method_decorator
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from infraohjelmointi_api.models import ProjectLocation
 from infraohjelmointi_api.serializers import ProjectLocationSerializer
+import uuid
 from rest_framework import status
-from django.http import StreamingHttpResponse
-from .utils import generate_response, generate_streaming_response
 
 from drf_yasg.utils import swagger_auto_schema
 
-
-@method_decorator(
-    name="list",
-    decorator=swagger_auto_schema(
-        operation_description="""
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_description="""
     `GET /api/locations/`
 
     Get all locations.
-
-    The projectLocation data on projects shows the lowest location category from the class hierarchy, and it might be empty.
-    To get detailed location information for projects, use the projectDistrict data and the endpoint `/api/districts/`.
     """
-    ),
-)
+))
 class ApiLocationsViewSet(BaseViewSet):
-    http_method_names = ["get"]
+    http_method_names = ['get']
 
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    queryset = ProjectLocation.objects.all()
     serializer_class = ProjectLocationSerializer
 
+
     @swagger_auto_schema(
-        operation_description="""
-        `GET /api/locations/{id}`
+            operation_description = """
+            `GET /api/locations/{id}`
 
-        Get specific location data.
+            Get a location.
 
-        The projectLocation data on projects shows the lowest location category from the class hierarchy, and it might be empty.
-        To get detailed location information for projects, use the projectDistrict data and the endpoint `/api/districts/`.
-        """,
-    )
+            The projectLocation data on projects shows the lowest location category from the class hierarchy, and it might be empty.
+            To get detailed location information for projects, use the projectDistrict data and the endpoint `/api/districts/`.
+            """,
+            )
     def retrieve(self, request, pk=None):
         try:
-            return generate_response(self, request.user.id, pk, request.path)
+            uuid.UUID(str(pk))
+            queryset = self.get_queryset()
+            obj = queryset.get(pk=pk)
+            serializer = self.get_serializer(obj)
+            return Response(serializer.data)
         except Exception:
             return Response(
                 data={"message": "Not found"}, status=status.HTTP_404_NOT_FOUND
             )
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        return StreamingHttpResponse(
-            generate_streaming_response(
-                queryset, self.serializer_class, request.user.id, request.path
-            ),
-            content_type="application/json",
-        )
