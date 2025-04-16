72c6cddfc896a804976636f9289ffbd9f8371f53
Milla Eerola <77616473+millaeerola@users.noreply.github.com>
2025-04-16 17:07:57 +0300

    Update ProjectViewSet.py

--- infraohjelmointi_api/views/ProjectViewSet.py
+++ infraohjelmointi_api/views/ProjectViewSet.py
@@ -1,4 +1,5 @@
-from datetime import date, timedelta, datetime
+from datetime import date, timedelta
+import datetime
 import logging
 import time
 from django_filters.rest_framework import DjangoFilterBackend
