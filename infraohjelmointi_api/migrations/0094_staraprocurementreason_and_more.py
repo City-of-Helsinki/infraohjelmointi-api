3c96e0aeffa1b366940fd9bb16eb16b56e7eb729
Sari <122358667+sarivah@users.noreply.github.com>
2026-03-19 09:23:35 +0200

    IO-823 Populated database with staraprocurementreason values

--- infraohjelmointi_api/migrations/0094_staraprocurementreason_and_more.py
+++ infraohjelmointi_api/migrations/0094_staraprocurementreason_and_more.py
@@ -27,3 +27,5 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.DO_NOTHING, to='infraohjelmointi_api.staraprocurementreason'),
         ),
     ]
+
+
