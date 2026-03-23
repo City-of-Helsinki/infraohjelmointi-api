a2105cf4bd231e1ec7068f0e17ef77096e7e7640
Sari <122358667+sarivah@users.noreply.github.com>
2026-03-23 14:33:03 +0200

    IO-407 Added migration for new order field data

--- infraohjelmointi_api/models/OrderedLookupModel.py
+++ infraohjelmointi_api/models/OrderedLookupModel.py
@@ -3,14 +3,14 @@ from django.db.models import Max
 
 
 class OrderedLookupModel(models.Model):
-    order = models.IntegerField(default=0)
+    order = models.IntegerField(null=True, blank=True)
 
     class Meta:
         abstract = True
         ordering = ["order"]
 
     def save(self, *args, **kwargs):
-        if self._state.adding and self.order == 0:
+        if self._state.adding and self.order is None:
             max_order = self.__class__.objects.aggregate(Max("order"))["order__max"]
             self.order = (max_order + 1) if max_order is not None else 0
         super().save(*args, **kwargs)
