a4ce833a1a3eb5d4116ce190729fb60e350af50e
Sari Vaheri <sari.vaheri@gofore.com>
2026-08-19 15:59:10 +0300

    feat(api): added new classes for service tunnel

---
+++ infraohjelmointi_api/migrations/0113_add_service_tunnel_classes.py
@@ -0,0 +1,75 @@
+from django.db import migrations
+
+
+MASTER_CLASS_NAME = "8 10 Suuret liikennehankkeet"
+MASTER_CLASS_PATH_ROOT = "8 10"
+
+CLASS_NAME = "8 10 05 Keskustan huoltotunnelin jatke"
+CLASS_PATH = f"{MASTER_CLASS_NAME}/{CLASS_NAME}"
+
+SUB_CLASS_NAME = "8 10 05 01 Keskustan huoltotunnelin jatke"
+SUB_CLASS_PATH = f"{CLASS_PATH}/{SUB_CLASS_NAME}"
+
+ALL_PATHS = [CLASS_PATH, SUB_CLASS_PATH]
+
+
+def _get_parent(project_class_model, parent_name):
+    parent = project_class_model.objects.filter(
+        name=parent_name,
+        path__startswith=MASTER_CLASS_PATH_ROOT,
+    ).first()
+    if not parent:
+        raise RuntimeError(
+            "Parent ProjectClass not found for "
+            f"name {parent_name} under numeric root {MASTER_CLASS_PATH_ROOT}"
+        )
+    return parent
+
+
+def add_service_tunnel_classes(apps, schema_editor):
+    project_class_model = apps.get_model("infraohjelmointi_api", "ProjectClass")
+
+    master_class = _get_parent(project_class_model, MASTER_CLASS_NAME)
+
+    new_class, _ = project_class_model.objects.get_or_create(
+        path=CLASS_PATH,
+        defaults={
+            "name": CLASS_NAME,
+            "parent": master_class,
+            "forCoordinatorOnly": False,
+            "relatedTo": None,
+            "relatedLocation": None,
+            "defaultProgrammer": None,
+        },
+    )
+
+    project_class_model.objects.get_or_create(
+        path=SUB_CLASS_PATH,
+        defaults={
+            "name": SUB_CLASS_NAME,
+            "parent": new_class,
+            "forCoordinatorOnly": False,
+            "relatedTo": None,
+            "relatedLocation": None,
+            "defaultProgrammer": None,
+        },
+    )
+
+
+def remove_service_tunnel_classes(apps, schema_editor):
+    project_class_model = apps.get_model("infraohjelmointi_api", "ProjectClass")
+    project_class_model.objects.filter(path__in=ALL_PATHS).delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("infraohjelmointi_api", "0112_merge_20260818_0905"),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            add_service_tunnel_classes,
+            remove_service_tunnel_classes,
+        ),
+    ]
