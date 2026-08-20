728306629a5d714de08ff479785738e5aeac1c5d
Santtu Nykänen <santtu.nykanen@gofore.com>
2026-08-20 13:11:13 +0200

    fix: add merge migration for IO-812 + develop convergence

---
+++ infraohjelmointi_api/migrations/0114_merge_20260820_1350.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("infraohjelmointi_api", "0112_noteimage"),
+        ("infraohjelmointi_api", "0113_remove_warranty_incomplete_detail"),
+    ]
+
+    operations = []
