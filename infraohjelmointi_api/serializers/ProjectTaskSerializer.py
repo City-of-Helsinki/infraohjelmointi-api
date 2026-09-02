3b76b596738ee57b94753955789c44ef69b3b958
Marko Haarni <marko.haarni@gofore.com>
2026-09-02 15:01:06 +0300

    feat: add endpoint for getting project tasks (IO-899)
    Project tasks are retrieved for the current user based on their role.
    
    Tasks include id, name, estPlanningStart, estPlanningEnd, estConstructionStart,
    estConstructionEnd, budget and constructionProcurementMethod from the project
    that requires some action along with the taskType.
    
    At this point there is only one task type of `NAME_CONSTRUCTION_PROJECT_MANAGER`.
    For that type all projects that have construction handover
    with a status `SUBMITTED_TO_CONSTRUCTION` are retrieved for users with
    construction management lead role (rakennuttamisen esihenkilöt).
    
    Other types of tasks are planned to be added later.

---
+++ infraohjelmointi_api/serializers/ProjectTaskSerializer.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from infraohjelmointi_api.models import Project
+from infraohjelmointi_api.serializers import (
+  ConstructionProcurementMethodSerializer
+)
+  
+
+TASK_TYPE_NAME_CONSTRUCTION_PROJECT_MANAGER = "NAME_CONSTRUCTION_PROJECT_MANAGER"
+
+# NOTE: It's planned that more task types will be added in the future
+TASK_TYPES = [
+    TASK_TYPE_NAME_CONSTRUCTION_PROJECT_MANAGER,
+]
+
+TASK_TYPE_CHOICES = [(task_type, task_type) for task_type in TASK_TYPES]
+
+class ProjectTaskSerializer(serializers.ModelSerializer):
+    constructionProcurementMethod = ConstructionProcurementMethodSerializer(read_only=True)
+    taskType = serializers.ChoiceField(choices=TASK_TYPE_CHOICES, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = (
+            'id',
+            'name',
+            'estPlanningStart',
+            'estPlanningEnd', 
+            'estConstructionStart',
+            'estConstructionEnd',
+            'budget',
+            'constructionProcurementMethod',
+            'taskType'
+        )
---
+++ infraohjelmointi_api/views/ProjectTasksViewSet.py
@@ -0,0 +1,57 @@
+from overrides import override
+from django.db.models import CharField, Value
+from helusers.oidc import ApiTokenAuthentication
+from rest_framework import mixins, status, viewsets
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from infraohjelmointi_api.models import ConstructionHandover, Project
+from infraohjelmointi_api.serializers.ProjectTaskSerializer import (
+    ProjectTaskSerializer,
+    TASK_TYPE_NAME_CONSTRUCTION_PROJECT_MANAGER,
+)
+from infraohjelmointi_api.permissions import IsConstructionManagementLead
+
+HANDOVER_STATUS_SUBMITTED_TO_CONSTRUCTION = "SUBMITTED_TO_CONSTRUCTION"
+
+
+class ProjectTasksViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """
+    Get the list of project tasks for the current user based on their role.
+    """
+
+    serializer_class = ProjectTaskSerializer
+    http_method_names = ["get"]
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [ApiTokenAuthentication, SessionAuthentication]
+
+    def _is_construction_management_lead(self, request):
+        return IsConstructionManagementLead().user_in_construction_management_lead_group(
+            request=request
+        )
+
+    @override
+    def get_queryset(self):
+        if self._is_construction_management_lead(self.request): 
+            # Get all projects that have a construction handover submitted to construction
+            handover_project_ids = ConstructionHandover.objects.filter(
+                status=HANDOVER_STATUS_SUBMITTED_TO_CONSTRUCTION
+            ).values_list("project_id", flat=True)
+
+            return (
+                Project.objects.filter(id__in=handover_project_ids)
+                .annotate(
+                    taskType=Value(
+                        TASK_TYPE_NAME_CONSTRUCTION_PROJECT_MANAGER, output_field=CharField()
+                    )
+                )
+            )
+        
+        return Project.objects.none()
+
+    @override
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
